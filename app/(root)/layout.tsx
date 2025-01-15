b41168c8fac28c474d8688b24c874984127b8ea5
abhishekmaniy <abhishekmaniyar502@gmail.com>
2025-01-16 00:38:44 +0530

    first commit- complete project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import { Toaster } from '@/components/ui/toaster'
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next'
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'YOOM',
+  description: 'Video Calling App',
+  icons:'icons/logo.svg'
+}
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}
+      <Toaster />
+
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
---
+++ constants/index.ts
@@ -0,0 +1,35 @@
+export const sidebarLinks = [
+  {
+    label: 'Home',
+    imageUrl: '/icons/Home.svg',
+    route: '/'
+  },
+  {
+    label: 'Upcoming ',
+    imageUrl: '/icons/upcoming.svg',
+    route: '/upcoming'
+  },
+  {
+    label: 'Previous',
+    imageUrl: '/icons/Previous.svg',
+    route: '/previous'
+  },
+  {
+    label: 'Recordings',
+    imageUrl: '/icons/Recordings.svg',
+    route: '/recordings'
+  },
+  {
+    label: 'Personal Room',
+    imageUrl: '/icons/add-personal.svg',
+    route: '/personal-room'
+  }
+]
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png'
+]
